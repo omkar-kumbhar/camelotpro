9799d1286bc84be88363ac64345d9e0989ebe159
akshowhini <udayrddy@gmail.com>
2019-09-24 22:06:16 -0400

    TODO list

---
+++ camelot_pro/BugBounty.py
@@ -0,0 +1,10 @@
+"""
+To add bug bounty program by default
+"""
+
+import requests
+
+
+# TODO 1. Route 500 Errors and let the user know something bad happened
+# TODO 2. Generate a presigned URL to PUT OBJECT
+# TODO 3. PUT THE OBJECT
